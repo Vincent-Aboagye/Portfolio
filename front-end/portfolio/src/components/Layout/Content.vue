783a1b20270a1239c4c115226b2b265ad744915f
Vynx <68636740+Vincent-Aboagye@users.noreply.github.com>
2020-12-11 16:56:24 +0000

    still on building my portfolio

--- front-end/portfolio/src/components/Layout/Content.vue
+++ front-end/portfolio/src/components/Layout/Content.vue
@@ -1,11 +1,10 @@
 <template>
       <v-parallax
-    dark
-    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
-  >
+    dark >
 <v-row
+    
     >
-      <v-col class="text-center" cols="12">
+      <v-col cols="12">
         <h1 class="display-1 font-weight-thin mb-4">Content Here</h1>
         <h4 class="subheading"></h4>
       </v-col>
--- front-end/portfolio/src/components/Layout/Header.vue
+++ front-end/portfolio/src/components/Layout/Header.vue
@@ -1,13 +1,15 @@
 <template>
+<div>
   <v-app-bar 
     app
-    elevation=3
     color="#fff">
+    <v-row>
     <v-container>
       <v-toolbar flat>
+        <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = true"></v-app-bar-nav-icon>
         <v-toolbar-title>Portfolio</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-toolbar-items>
+
+        <v-toolbar-items class="hidden-sm-and-down text-center" >
         <v-btn text>
           <v-icon >mdi-home-outline</v-icon>
           Home
@@ -23,9 +25,52 @@
            About 
         </v-btn>
         </v-toolbar-items>
+  
       </v-toolbar>
+      
     </v-container>
+    
   </v-app-bar>
+  <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="gray--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-contacts</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+
+            <v-list-item-icon>
+              <v-icon>mdi-information-variant</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+</div>
+  
 </template>
 
 
